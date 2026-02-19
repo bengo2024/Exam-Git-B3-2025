703ddaa68ddb44990531ce25ab02d7d9f1ab4d59
bengo2024 <andrethomasbengon12@gmail.com>
2026-02-19 14:24:00 +0100

    Ajout de la logique de login dans feat/login

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+def login(username, password):
+    if username == "admin" and password == "bengo":
+        print("Connexion réussi")
+    else:
+        print("Connexion échoué")
+
 print("Hello World")
+login("admin", "bengo")  # Test
